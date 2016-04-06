e4378296338393a4ea2f3a687164f6efb99e8b90
yuliya.pshinko <yuliya.pshinko>
2016-04-06 10:53:45 +0300

    first commit

---
+++ Bank/Bank/StringCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorNS
+{
+    /// <summary> 
+    /// Bank Account demo class. 
+    /// </summary> 
+    public class StringCalculator
+    {
+        public int sum = 0;
+
+        public StringCalculator()
+        {
+        }
+
+        public void Add (string numbers)
+        {
+            string pattern = ",";
+            string[] numbersArray = Regex.Split(numbers, pattern);
+            if (numbersArray.Length > 0)
+            {
+                for (int i = 0; i < numbersArray.Length; i++)
+                {
+                    sum += Int32.Parse(numbersArray[i]);
+                }
+            }
+            System.Console.WriteLine(sum);
+        }
+
+    }
+}
---
+++ Bank/BankTests/Debit.cs
@@ -0,0 +1,45 @@
+﻿// unit test code
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculatorNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        // unit test code
+        [TestMethod]
+        public void SumOfEmptyString()
+        {
+            // arrange
+            int expected = 0;
+            string numbers = "";
+            StringCalculator calc = new StringCalculator();
+
+            // act
+            calc.Add(numbers);
+
+            // assert
+            int actual = calc.sum;
+            Assert.AreEqual(expected, actual, "Sum of empty string is wrong");
+        }
+
+        // unit test code
+        [TestMethod]
+        public void SumOfOneArgument()
+        {
+            // arrange
+            int expected = 1;
+            string numbers = "1";
+            StringCalculator calc = new StringCalculator();
+
+            // act
+            calc.Add(numbers);
+
+            // assert
+            int actual = calc.sum;
+            Assert.AreEqual(expected, actual, "Sum of '1' is wrong");
+        }
+    }
+}

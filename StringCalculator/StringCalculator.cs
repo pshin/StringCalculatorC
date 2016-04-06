4f3a05c380c71791cf90057d29fe05ecd3da950d
pshin <iuliiap@yandex.ru>
2016-04-06 15:18:34 +0300

    StringCalculator. Included tools which is used in ReportingApp

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringCalculatorNS
+{
+    public class StringCalculator
+    {
+        public string Pattern = ",";
+        public int Sum = 0;
+
+        public StringCalculator()
+        {
+        }
+
+        public int Add (string numbers)
+        {
+            if (!String.IsNullOrEmpty(numbers)) {
+                Array.ForEach(Regex.Split(numbers, Pattern), number =>
+                {
+                    Sum += Int32.Parse(number);
+                });
+            }
+            return Sum;
+        }
+
+    }
+}
---
+++ StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿// unit test code
+using NUnit.Framework;
+using FluentAssertions;
+using StringCalculatorNS;
+
+namespace StringCalculatorTests
+{
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void should_count_sum_of_empty_string_as_zero()
+        {
+            // arrange
+            const string numbers = "";
+            var calc = new StringCalculator();
+
+            // act
+            var actual = calc.Add(numbers);
+
+            // assert
+            actual.Should().Be(0);
+        }
+    }
+}

43a83133ae11a1b656532f194b235385f2a50689
pshin <iuliiap@yandex.ru>
2016-04-19 13:03:48 +0300

    StringCalculator. Step 9. Kata 1 finished

--- StringCalculatorTests/StringCalculatorTests.cs
+++ StringCalculatorTests/StringCalculatorTests.cs
@@ -15,6 +15,8 @@ namespace StringCalculatorTests
         [TestCase("1,2,3", 6)]
         [TestCase("1\n2,3", 6)]
         [TestCase("//;\n1;2", 3)]
+        [TestCase("//[***]\n1***2***3", 6)]
+        [TestCase("//[*][%]\n1*2%3", 6)]
         public void should_count_sum_of_numbers_in_string(string numbers, int expected)
         {
             // arrange
